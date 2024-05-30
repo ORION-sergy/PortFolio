39cdb9aeb72c7603ad4525361941a3034f6cd897
sergio <ramos_sergio20@hotmail.com>
2024-05-30 12:30:27 -0300

    link local-remote

---
+++ ejemplo.py
@@ -0,0 +1,23 @@
+print ("trying classic hi world")
+"""for()"""
+#for(){}
+print('--- ---')
+#print(9 + '2')""" data tpe operation """
+print('for(i = 0; i < 2; i++){return}')
+print('program finished')
+print( 9 + 9)
+print('9' + '9')
+print(9, '9')#operador concatenador
+print(9 + int('9'))
+print(str(9) + '9')
+
+"""
+ 
+str() convierte a string
+int() convierte a integer
+bool() convierte a booleano
+float() convierte a flotante
+
+list() convierte a arreglo
+
+"""
---
+++ scripts.js
@@ -0,0 +1,96 @@
+document.addEventListener("DOMContentLoaded", function(){
+/* toggle icon bar */
+    let menuIcon = document.querySelector('#menu-icon');
+    let navbar = document.querySelector('.navbar');
+
+    menuIcon.onclick = ()=>{
+        menuIcon.classList.toggle('bx-x');
+        navbar.classList.toggle('active');
+    }    
+/* scroll section active links */
+    let sections = document.querySelectorAll('section');
+    let navLinks = document.querySelectorAll('header nav a');
+
+    window.onscroll = () =>{
+        sections.forEach(sec=>{
+            let top = window.scrollY;
+            let offset  = sec.offsetTop -150;
+            let height = sec.offsetHeight;
+            let id = sec.getAttribute('id');
+
+            if (top >= offset && top < offset + height) {
+                navLinks.forEach(link => {
+                    link.classList.remove('active');
+                });
+                document.querySelector(`header nav a[href*='${id}']`).classList.add('active');
+            }
+        });
+    };
+
+    /* sticky navbar */
+
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+    /* it aint work calling var in the old method */
+    /* remove toggle icon and navbar when click navbar link (scrol) */
+    /* menuIcon.classList.remove('bx-x');
+        navbar.classList.remove('active'); */
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            menuIcon.classList.remove('bx-x');
+            navbar.classList.remove('active');
+        });
+    });
+
+    /* scrollreveal */
+    ScrollReveal({
+        //reset: true,
+        distance: '80px',
+        duration: 2000,
+        delay: 200
+    });
+
+    ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+    ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+    ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+    ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+    /* typed js */
+    const typed = new Typed('.multiple-text', {
+        strings: ['Frontend Developer','Freelance &','TeamWork'],
+        typeSpeed: 100,
+        backSpeed: 100,
+        backDelay: 1000,
+        loop: true
+    })
+
+/* ---- */
+    let puntaje = (
+        function(){
+            let puntaje_Jugador = 0;
+
+            function goldenApple(){
+                return puntaje_Jugador += 25;
+            }
+            function greenApple(){
+                return puntaje_Jugador += 10;
+            }
+            function cactus(){
+                return puntaje_Jugador -= 5;
+            }
+            function actualPoint (){
+                return puntaje_Jugador;
+            }
+            return{
+                goldenApple,
+                goldenApple,
+                cactus,
+                actualPoint
+            }
+        }
+    )()
+    console.log(puntaje.goldenApple());
+    console.log(puntaje.cactus());
+    console.log(puntaje.actualPoint());
+});
